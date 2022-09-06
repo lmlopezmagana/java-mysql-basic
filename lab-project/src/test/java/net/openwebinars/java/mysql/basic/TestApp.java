c5da2422920e8ceda1d358b0a4e01340a78626cc
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-09-06 08:16:57 +0200

    Proyecto base

---
+++ lab-project/src/test/java/net/openwebinars/java/mysql/basic/TestApp.java
@@ -0,0 +1,15 @@
+package net.openwebinars.java.mysql.basic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestApp {
+
+
+    @Test
+    public void testSomething() {
+        assertTrue(true);
+    }
+
+}
