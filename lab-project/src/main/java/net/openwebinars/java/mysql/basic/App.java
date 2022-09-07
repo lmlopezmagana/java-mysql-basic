8ee3aaefe47c16749e736fdb08046e038838a9df
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-09-07 11:08:24 +0200

    v2-conexion

--- lab-project/src/main/java/net/openwebinars/java/mysql/basic/App.java
+++ lab-project/src/main/java/net/openwebinars/java/mysql/basic/App.java
@@ -1,9 +1,26 @@
 package net.openwebinars.java.mysql.basic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String url = """
+            jdbc:mysql://localhost/database?allowPublicKeyRetrieval=true&useSSL=false
+            """;
+
+        try {
+            Connection conn = DriverManager.getConnection(url,"user", "password");
+            System.out.println("Conectado a la base de datos");
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("ErrorCode: " + ex.getErrorCode());
+        }
     }
 
 }
