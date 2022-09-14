6400ca369c5ceb637e1cf2bdcd4486fef07ec34a
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-09-14 10:39:07 +0200

    v4-select

--- lab-project/src/main/java/net/openwebinars/java/mysql/basic/App.java
+++ lab-project/src/main/java/net/openwebinars/java/mysql/basic/App.java
@@ -28,6 +28,22 @@ public class App {
                 System.out.println("Se ha insertado el registro en la base de datos");
 
 
+            String sql2 = """
+                    SELECT * FROM alumno;
+                    """;
+
+            ResultSet rs = stm.executeQuery(sql2);
+
+            while (rs.next()) {
+                System.out.println(String.format("%d. %s %s %d %s",
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getString(5)));
+            }
+
+
             conn.close();
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
